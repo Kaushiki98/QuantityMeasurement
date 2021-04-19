b2fc235c59a954165432813ad094a43bec76b806
kaushiki <kaushiki.ak88@gmail.com>
2021-04-19 22:22:09 +0530

    ADDED:comparing yard and feet

---
+++ Src/QuantityMeasurement.js
@@ -0,0 +1,32 @@
+/*************************************************************
+ *
+ * Execution       : default node cmd> node QuantityMeasurement.js
+ * Purpose         : Unit Measurement Conversion
+ *
+ * @description    : Convert and compare the lengths 
+ *                   of different measurements. 
+ *
+ * @file           : QuantityMeasurement.js
+ * @overview       : Compare, convert and add quantities
+ * @module         : Node.js, Mocha and Chai and local packages
+ *                   are added.
+ * @version        : 1.0
+ * @since          : 19/04/2021
+ *
+ * **********************************************************/
+
+/**
+ * @description Class QuantityMeasurement
+ * @class QuantityMeasurement
+ */
+ class QuantityMeasurement {
+
+  feet = (value) => {
+    return value * 12;
+ };
+
+  yard = (value) => {
+    return value * 36
+  };
+}
+module.exports = QuantityMeasurement;
---
+++ Test/QuantityMeasurement.test.js
@@ -0,0 +1,15 @@
+var assert = require('chai').assert;
+var quantity = require('../Src/QuantityMeasurement')
+
+describe('Quantity Measurement for lenght', () => {
+
+  var length;
+  before(function () {
+    length = new quantity();
+  });
+
+  it('Given 3 feet and 1 yard should return equal', () => {
+    assert.equal(length.yard(1), length.feet(3));
+  });
+
+});
